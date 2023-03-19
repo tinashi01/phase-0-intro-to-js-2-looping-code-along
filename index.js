654c65956d9b23ad5f13303e9e660804dced8753
Tina Shi <init.defaultBranch>
2023-03-19 19:18:34 +1100

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names, event = "surprise") {
+    const newNames = [];
+    for (let i=0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        newNames.push(message);
+    }
+    return newNames;
+}
+
+function countDown(i) {
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
